55423790e5f5d5b973391b8e8ecea0cc14b78fa1
Kevin McLaughlin <kevin.mclaughlin70@gmail.com>
2016-02-03 07:36:45 -0600

    updated transpose table

--- transpose_table.js
+++ transpose_table.js
@@ -10,26 +10,26 @@ function create_flip_table () {
     /*Loops through all tables with the class "flip_table" into a flip table*/
     $('.flip_table').each(function(){
         var parent = $(this).parent();
-        var original = $(this).html();
+        var new_table = $(this).clone().empty();
+        $(this).addClass('hidden-xs');
         
-        //Update current table
-        $(this).addClass('visible-xs')
-        var $this = $(this);
         var newrows = [];
-        $this.find("tr").each(function(){
+        $(this).children().first().find("tr").each(function(){
             var i = 0;
-            $(this).find("td").each(function(){
+            $(this).children().each(function(){
+                if (newrows.length <= i){
+                    newrows.push($('<tr></tr>'));
+                }
+                newrows[i].append($(this)[0].outerHTML);
                 i++;
-                if(newrows[i] === undefined) { newrows[i] = $("<tr></tr>"); }
-                newrows[i].append($(this));
             });
         });
-        $this.find("tr").remove();
-        $.each(newrows, function(){
-            $this.append(this);
-        });
         
-        //Append original
-        $(parent).append('<table class="flip_table hidden-xs">'+original+"</table>")
+        for (i = 0; i < newrows.length; i++) {
+            var new_row = newrows[i];
+            new_table.append(new_row);
+        }
+        $(new_table).addClass('visible-xs');
+        parent.append(new_table);
     })
 }
